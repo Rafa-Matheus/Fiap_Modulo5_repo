61f2bfcc4d1a90edf6beb3646f0eed08c6a4219e
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-13 14:34:02 -0300

    Create ContatoModel.cs

---
+++ Fiap.Web.Donation1/Models/ContatoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Models
+{
+    public class ContatoModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
