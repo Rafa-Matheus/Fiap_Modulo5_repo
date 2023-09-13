caea8cbf4633322ebfa5a28c0218d01382096449
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-13 14:34:09 -0300

    Create ContatoController.cs

---
+++ Fiap.Web.Donation1/Controllers/ContatoController.cs
@@ -0,0 +1,30 @@
+﻿using Fiap.Web.Donation1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ContatoModel contatoModel)
+        {
+            return View("Sucesso"); // Para passar uma View específica
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View(); // RedirectToAction("Index", "Home");
+        }
+    }
+}
