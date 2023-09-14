80ea5c0c9165ee95a042818bffbd89ccaf7f01f3
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-14 11:05:13 -0300

    Create ProdutoController.cs

---
+++ Fiap.Web.Donation1/Controllers/ProdutoController.cs
@@ -0,0 +1,77 @@
+﻿using Fiap.Web.Donation1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private List<ProdutoModel> produtos;
+
+        public ProdutoController()
+        {
+            // Acesso fake ao banco dados
+
+            produtos = new List<ProdutoModel>{
+                new ProdutoModel()
+                {
+                    ProdutoId = 1,
+                    Nome = "Iphone 11",
+                    TipoProdutoId = 1,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+                new ProdutoModel()
+                {
+                    ProdutoId = 2,
+                    Nome = "Iphone 12",
+                    TipoProdutoId = 2,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+                new ProdutoModel()
+                {
+                    ProdutoId = 3,
+                    Nome = "Iphone 13",
+                    TipoProdutoId = 1,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Consulta o BD
+
+            ViewBag.Produtos = produtos;
+            //TempData["Produtos"] = produtos;
+
+            return View();
+        }
+
+        [HttpGet] // Get é para obter/abrir/consultar algo
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost] // Post deve ser usado para gravar algo
+        public IActionResult Novo(ProdutoModel produtoModel)
+        {
+            // Aqui poderiam ir as instruções que gravariam o produto no DB
+
+            ViewBag.Produtos = produtos;
+
+            //ViewBag.Mensagem = $"{produtoModel.Nome} cadastrado com sucesso";
+            TempData["Mensagem"] = $"{produtoModel.Nome} cadastrado com sucesso";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
