5985f6c28638a9d5efba6971fbff13b78153f07b
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-14 11:02:46 -0300

    Create UsuarioModel.cs

---
+++ Fiap.Web.Donation1/Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Models
+{
+    public class UsuarioModel
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
