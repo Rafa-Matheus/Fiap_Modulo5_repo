b4b8d6f330eb88cf43dbf0412358679ce344b45f
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-20 18:51:45 -0300

    Create DataContext.cs

---
+++ Fiap.Web.Donation1/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Fiap.Web.Donation1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Data
+{
+    // Essa é a classe que manipula os dados da aplicação
+    public class DataContext : DbContext
+    {
+        public DbSet<TipoProdutoModel> TipoProdutos { get; set; }
+
+        /*
+        public DbSet<ProdutoModel> Produtos { get; set; }
+        public DbSet<TrocaModel> Trocas { get; set; }
+        */
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected DataContext()
+        {
+
+        }
+    }
+}
