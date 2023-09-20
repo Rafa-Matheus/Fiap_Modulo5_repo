8755015953e4dbd1387285fe4e8c5103f4d115e4
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-20 18:53:55 -0300

    Migration files created

---
+++ Fiap.Web.Donation1/Migrations/20230919231923_MigracaoInicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fiap.Web.Donation1.Migrations
+{
+    public partial class MigracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoProduto",
+                columns: table => new
+                {
+                    TipoProdutoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoProduto", x => x.TipoProdutoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoProduto");
+        }
+    }
+}
