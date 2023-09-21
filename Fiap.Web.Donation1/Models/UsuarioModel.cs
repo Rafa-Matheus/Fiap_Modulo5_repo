79ef1d98caddef4e5930b8f08f8c6a76c12f5243
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-21 17:37:07 -0300

    Update UsuarioModel.cs

--- Fiap.Web.Donation1/Models/UsuarioModel.cs
+++ Fiap.Web.Donation1/Models/UsuarioModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace Fiap.Web.Donation1.Models
 {
     public class UsuarioModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Notação para gerar Id automático e incremental
         public int UsuarioId { get; set; }
+
+        [StringLength(50)]
         public string Nome { get; set; }
+
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
