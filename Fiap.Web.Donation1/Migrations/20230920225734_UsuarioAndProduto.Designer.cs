4dbf1b7797a26dc0c7f641ade20057698c2f4dbe
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-21 17:33:29 -0300

    Added UsuarioAndProduto Migration

--- Fiap.Web.Donation1/Migrations/20230920225734_UsuarioAndProduto.Designer.cs
+++ Fiap.Web.Donation1/Migrations/20230920225734_UsuarioAndProduto.Designer.cs
@@ -4,14 +4,16 @@ using Fiap.Web.Donation1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fiap.Web.Donation1.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230920225734_UsuarioAndProduto")]
+    partial class UsuarioAndProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Fiap.Web.Donation1/Migrations/20230920225734_UsuarioAndProduto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fiap.Web.Donation1.Migrations
+{
+    public partial class UsuarioAndProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Disponivel = table.Column<bool>(type: "bit", nullable: false),
+                    TipoProdutoId = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    SugestaoTroca = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataExpiracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.ProdutoId);
+                    table.ForeignKey(
+                        name: "FK_Produto_TipoProduto_TipoProdutoId",
+                        column: x => x.TipoProdutoId,
+                        principalTable: "TipoProduto",
+                        principalColumn: "TipoProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produto_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_TipoProdutoId",
+                table: "Produto",
+                column: "TipoProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_UsuarioId",
+                table: "Produto",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
