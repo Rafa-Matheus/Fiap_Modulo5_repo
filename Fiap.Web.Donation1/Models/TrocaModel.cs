891857b7c68b9e87efb2a36271b2bd1e4f2d7cba
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-09-27 18:54:19 -0300

    Create TrocaModel.cs

---
+++ Fiap.Web.Donation1/Models/TrocaModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.Donation1.Models
+{
+    public enum TrocaStatus
+    {
+        Iniciado = 1,
+        Analisado = 2,
+        Finalizado = 3,
+        Revertido = 4
+    }
+
+    [Table("Troca")]
+    public class TrocaModel
+    {
+        [Key]
+        public Guid TrocaId { get; set; } = Guid.NewGuid();
+
+        public DateTime DataCriacao { get; set; }
+
+        public TrocaStatus Status { get; set; }
+
+        public int ProdutoId1 { get; set; }
+
+        [ForeignKey(nameof(ProdutoId1))]
+        public ProdutoModel ProdutoModel1 { get; set; }
+
+        public int ProdutoId2 { get; set; }
+
+        [ForeignKey(nameof(ProdutoId2))]
+        public ProdutoModel ProdutoModel2 { get; set; }
+    }
+}
